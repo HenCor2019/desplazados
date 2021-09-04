10d2431eb41ab99272fa268d8b4023b93ff1d290
Carolina Carranza <cmcc1505@gmail.com>
2021-09-03 20:35:09 -0600

    Update component widths

--- src/Section.js
+++ src/Section.js
@@ -1,9 +1,9 @@
 function Section(props) {
     return (
         <div className="Section flex">
-            <div className="h-screen w-80 bg-right bg-auto bg-cover bg-no-repeat min-0" style={{ backgroundImage: `url(${props.img})` }}>
+            <div className="h-screen w-72 bg-right bg-auto bg-cover bg-no-repeat min-0" style={{ backgroundImage: `url(${props.img})` }}>
             </div>
-            <div className="w-4/5">
+            <div className="w-5/6">
                 <nav className="main-nav flex justify-end p-4">
                     {/* Los href son temporales */}
                     <a href={"./index.js"} className="px-4 font-sans text-gray-500 hover:text-gray-400">Inicio</a>
