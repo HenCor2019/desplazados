0008902818fbf6b696e36a08c3056e9885dacd9c
Henry Cortez <Henry200amaya@gmail.com>
2021-09-05 15:27:47 -0600

    Add Banner components and causes components

--- src/Containers/Causes/index.js
+++ src/Containers/Causes/index.js
@@ -8,7 +8,7 @@ import '../../components/CauseImage/index.css'
 
 const styles = {
   mainContainer:
-    'p-0 sm:p-4 h-full sm:h-3/4 md:h-5/6 w-full mt-9 md:mt-10 sm:justify-center sm:content-center lg:mt-6 grid grid-cols-1 sm:grid-cols-2 gap-4'
+    'p-0 sm:p-7 h-full sm:h-3/4 md:h-5/6 w-full mt-9 md:mt-10 sm:justify-center sm:content-center lg:mt-12 grid grid-cols-1 sm:grid-cols-2 gap-4'
 }
 
 export default function Causes() {
