5625a969c5f48db6ca18e38e9d08b8952e0ed176
Carolina Carranza <cmcc1505@gmail.com>
2021-09-05 03:48:32 -0600

    Add Looking for support main section

--- src/Section.js
+++ src/Section.js
@@ -1,20 +1,23 @@
+import SupportMainSect from "./SupportMainSect"
+
+const aClasses = "px-4 font-sans text-gray-500 hover:text-gray-400";
+
 function Section(props) {
     return (
         <div className="Section flex">
-            <div className="h-screen w-72 bg-right bg-auto bg-cover bg-no-repeat min-0" style={{ backgroundImage: `url(${props.img})` }}>
+            <div className="w-1/4 h-screen bg-right bg-auto bg-cover bg-no-repeat" style={{ backgroundImage: `url(${props.img})` }}>
             </div>
-            <div className="w-5/6">
+            <div className="w-3/4">
                 <nav className="main-nav flex justify-end p-4">
                     {/* Los href son temporales */}
-                    <a href={"./index.js"} className="px-4 font-sans text-gray-500 hover:text-gray-400">Inicio</a>
-                    <a href={"./index.js"} className="px-4 font-sans text-gray-500 hover:text-gray-400">Causas</a>
-                    <a href={"./index.js"} className="px-4 font-sans text-gray-500 hover:text-gray-400">Tiempo de espera</a>
-                    <a href={"./index.js"} className="px-4 font-sans text-gray-500 hover:text-gray-400">Los sueños</a>
-                    <a href={"./index.js"} className="px-4 font-sans text-gray-500 hover:text-gray-400">En busca de apoyo</a>
+                    <a href="./index.js" className={aClasses}>Inicio</a>
+                    <a href="./index.js" className={aClasses}>Causas</a>
+                    <a href="./index.js" className={aClasses}>Tiempo de espera</a>
+                    <a href="./index.js" className={aClasses}>Los sueños</a>
+                    <a href="./index.js" className={aClasses}>En busca de apoyo</a>
                 </nav>
-                <main>
-                    {/* Contenido principal */}
-                </main>
+                {/* Contenido principal */}
+                <SupportMainSect></SupportMainSect>
             </div>
         </div>
     )
