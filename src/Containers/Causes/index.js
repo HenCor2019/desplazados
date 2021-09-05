98f679e2af3d98609bf7d545c08078b79b11e8d2
Henry Cortez <Henry200amaya@gmail.com>
2021-09-05 00:53:56 -0600

    Add causes section structure

---
+++ src/Containers/Causes/index.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { MESSAGE } from '../../constants/FooterMessages'
+import { ReactComponent as Gallery } from '../../images/Causes/gallery.svg'
+import { ReactComponent as Story } from '../../images/Causes/story.svg'
+import FooterMessage from '../../components/FooterMessage/FooterMessage'
+import CauseImage from '../../components/CauseImage/CauseImage'
+import '../../components/CauseImage/index.css'
+
+const styles = {
+  mainContainer:
+    'p-0 sm:p-4 h-full sm:h-3/4 md:h-5/6 w-full mt-9 md:mt-10 sm:justify-center sm:content-center lg:mt-6 grid grid-cols-1 sm:grid-cols-2 gap-4'
+}
+
+export default function Causes() {
+  const [galleryGrayscale, setGalleryGrayscale] = useState(false)
+  const [storiesGrayscale, setStoriesGrayscale] = useState(false)
+
+  const handleMouseEnter = (save) => {
+    save(true)
+  }
+
+  const handleMouseLeave = () => {
+    setStoriesGrayscale(false)
+    setGalleryGrayscale(false)
+  }
+
+  return (
+    <main className={styles.mainContainer}>
+      <CauseImage
+        Image={Story}
+        handleMouseEnter={handleMouseEnter}
+        handleMouseLeave={handleMouseLeave}
+        grayscale={storiesGrayscale}
+        setGrayscale={setGalleryGrayscale}
+      />
+
+      <CauseImage
+        Image={Gallery}
+        handleMouseEnter={handleMouseEnter}
+        handleMouseLeave={handleMouseLeave}
+        grayscale={galleryGrayscale}
+        setGrayscale={setStoriesGrayscale}
+      />
+
+      <FooterMessage message={MESSAGE} />
+    </main>
+  )
+}
