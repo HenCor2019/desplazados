55784df921e96039344c3b00c05594be48f9c655
Carolina Carranza <cmcc1505@gmail.com>
2021-09-07 19:47:55 -0600

    Fix Support section paragraph's line height

--- src/SupportMainSect.js
+++ src/SupportMainSect.js
@@ -33,7 +33,7 @@ function SupportMainSect() {
                 <img src={Statistics} style={style2} className="object-contain w-full md:w-3/8 cursor-pointer" onMouseEnter={() => setStatisticsHover(true)} onMouseLeave={() => setStatisticsHover(false)} loading="lazy" />
             </div>
             <div className="flex w-full md:col-span-2 p-3 md:p-0">
-                <p className="text-justify md:pr-20 md:mt-10">Luego de la sentencia 411-2017 de la Corte Suprema de Justicia de El Salvador, el Estado se vio obligado a reconocer y legislar en favor de las víctimas de desplazamiento forzado por violencia. Tres años después, se aprobó una ley que carece de reglamento. Aunque existen acuerdos internacionales, como la Convención sobre Refugiados de 1951 y las Declaraciones de Cartagena y de Brasil, en El Salvador son documentos muertos. ¿A qué se enfrentan las familias cuando buscan apoyo de parte del Estado?</p>
+                <p className="text-justify md:pr-20 md:mt-8 leading-5">Luego de la sentencia 411-2017 de la Corte Suprema de Justicia de El Salvador, el Estado se vio obligado a reconocer y legislar en favor de las víctimas de desplazamiento forzado por violencia. Tres años después, se aprobó una ley que carece de reglamento. Aunque existen acuerdos internacionales, como la Convención sobre Refugiados de 1951 y las Declaraciones de Cartagena y de Brasil, en El Salvador son documentos muertos. ¿A qué se enfrentan las familias cuando buscan apoyo de parte del Estado?</p>
             </div>
             <div className="flex w-full items-start p-3 md:p-0">
                 <img src={Report} style={style3} className="object-contain w-full md:w-3/4 cursor-pointer" onMouseEnter={() => setReportHover(true)} onMouseLeave={() => setReportHover(false)} loading="lazy" />
