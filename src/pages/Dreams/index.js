948ccee5736abc7c280f9759368a9b2c37c308d3
massana2110 <00072416@uca.edu.sv>
2021-09-15 17:54:16 -0600

    Apply responsive to Dreams View

--- src/pages/Dreams/index.js
+++ src/pages/Dreams/index.js
@@ -1,12 +1,14 @@
 import Podcast from '../../assets/images/Dreams/podcast-dreams.png';
 import Gallery from '../../assets/images/Dreams/gallery-dreams.png';
 import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
 
-const styles = {};
+import Image from '../../Components/WaitingTime/Image';
 
 export default function Dreams() {
   const [isPodcastHover, setIsPodcastHover] = useState(false);
   const [isGalleryHover, setIsGalleryHover] = useState(false);
+  const {push} = useHistory();
 
   let stylePodcast, styleGallery;
   const grayscale = {filter: 'grayscale(1)'};
@@ -14,41 +16,50 @@ export default function Dreams() {
   if (isPodcastHover) styleGallery = grayscale;
   else if (isGalleryHover) stylePodcast = grayscale;
 
+  const handleOnDrawsGallery = () => {
+    push('/sueños/galeria');
+  };
+
+  const handleOnPodcast = () => {
+    push('/sueños/podcast');
+  };
+
   return (
-    <main className="grid grid-cols-1 grid-flow-row lg:grid-cols-2 lg:grid-rows-4 gap-0 p-10">
-      <div className="flex w-full justify-center items-center row-span-2 col-start-1 col-end-2">
-        <img
-          src={Podcast}
-          alt="Podcast"
-          style={stylePodcast}
-          className="animate-flip object-contain w-full md:w-2/3 2xl:w-1/2 md:min-w-support-image cursor-pointer"
+    <main className="sm:flex sm:min-w-0 sm:justify-center sm:items-center h-full">
+      <section className="grid grid-cols-1 grid-flow-row gap-0 h-full md:grid-cols-2  md:mt-20 md:p-6 lg:mt-0 lg:p-0 lg:gap-4 lg:relative">
+        <Image
+          img={Podcast}
+          alt={'Podcast'}
+          grayscale={stylePodcast}
           onMouseEnter={() => setIsPodcastHover(true)}
           onMouseLeave={() => setIsPodcastHover(false)}
-          loading="lazy"
+          handleOnClick={handleOnPodcast}
+          position={'cursor-pointer items-center justify-center lg:row-start-2'}
         />
-      </div>
-      <div className="flex w-full items-center row-start-2 row-end-4 col-start-2 col-end-3">
-        <img
-          src={Gallery}
-          alt="Galería"
-          style={styleGallery}
-          className="animate-flip object-contain w-full md:w-2/3 2xl:w-1/2 md:min-w-support-image cursor-pointer"
+
+        <Image
+          img={Gallery}
+          alt={'Galería'}
+          grayscale={styleGallery}
           onMouseEnter={() => setIsGalleryHover(true)}
           onMouseLeave={() => setIsGalleryHover(false)}
-          loading="lazy"
+          handleOnClick={handleOnDrawsGallery}
+          position={
+            'cursor-pointer items-center justify-center mt-6 mb-6 md:mt-0 md:mb-0 lg:row-start-2 lg:col-start-2 lg:absolute lg:top-44'
+          }
         />
-      </div>
-      {/* <div></div> */}
-      <div className="flex justify-center items-start row-start-3 row-end-5 col-start-1 col-end-2 p-6">
-        <p className="text-justify leading-5 md:w-2/3 2xl:w-1/2">
-          Soñar es imaginar algo que se persigue o se anhela con mucho deseo,
-          que probablemente difiera en gran medida de la realidad, pero que
-          condiciona la voluntad de trabajar en la construcción de ese ideal.
-          Soñar es lo que mantiene con vida a muchas personas que han sido
-          víctimas de desplazamiento forzado, pero ¿cómo sueñan el futuro los
-          niños?, ¿qué futuro esperan los padres para sus hijos?
-        </p>
-      </div>
+
+        <div className="bg-red-600 md:bg-transparent order-1 text-white md:text-black flex justify-center items-center md:items-start md:w-full md:col-span-2 lg:col-span-1 lg:row-start-3 lg:-mt-16 2xl:-mt-24">
+          <p className="text-sm sm:text-base text-justify leading-snug p-4 md:pt-4 lg:p-8 lg:max-w-md">
+            Soñar es imaginar algo que se persigue o se anhela con mucho deseo,
+            que probablemente difiera en gran medida de la realidad, pero que
+            condiciona la voluntad de trabajar en la construcción de ese ideal.
+            Soñar es lo que mantiene con vida a muchas personas que han sido
+            víctimas de desplazamiento forzado, pero ¿cómo sueñan el futuro los
+            niños?, ¿qué futuro esperan los padres para sus hijos?
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
