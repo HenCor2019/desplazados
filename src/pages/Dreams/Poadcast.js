297c3705dc19b72aac4447b3b40d70d52afd83ba
Batarse1 <00014419@uca.edu.sv>
2021-09-17 05:16:23 -0600

    Add responsive design

--- src/pages/Dreams/Poadcast.js
+++ src/pages/Dreams/Poadcast.js
@@ -39,88 +39,93 @@ const poadcastPlayerStyle = {
 };
 
 export default function Poadcast() {
-
   const audios = [
     {
       image: NuevaOportunidadImg,
       src: NuevaOportunidadAudio,
       title: '"Una nueva oportunidad"',
-      index: 0
+      index: 0,
     },
     {
       image: VidaNormalImg,
       src: VidaNormalAudio,
       title: '"Deseo llevar una vida normal, como antes"',
-      index: 1
+      index: 1,
     },
     {
       image: PazTranquilidadImg,
       src: PazTranquilidadAudio,
       title: '"Paz, tranquilidad y seguridad"',
-      index: 2
+      index: 2,
     },
     {
       image: PermisoImg,
       src: PermisoAudio,
       title: '"Cuando tenga el permiso de trabajar, luchar por mi vida"',
-      index: 3
+      index: 3,
     },
     {
       image: SalirAdelanteImg,
       src: SalirAdelanteAudio,
       title: '"Tenemos que salir adelante porque nos tenemos la una a la otra"',
-      index: 4
-    }
+      index: 4,
+    },
   ];
 
   const [activeAudio, setActiveAudio] = useState(audios[0]);
   const [isRandomAudio, setIsRandomAudio] = useState(false);
 
-
   const onEnded = (current) => {
-    if(isRandomAudio){
-      setActiveAudio(audios[Math.trunc( Math.random() * (audios.length - 1) + 0)] )
-      setIsRandomAudio(false)
-      return
+    if (isRandomAudio) {
+      setActiveAudio(
+        audios[Math.trunc(Math.random() * (audios.length - 1) + 0)]
+      );
+      setIsRandomAudio(false);
+      return;
     }
 
-    if (current.index === 4){
-      setActiveAudio(audios[0])
-      setIsRandomAudio(false)
-      return
+    if (current.index === 4) {
+      setActiveAudio(audios[0]);
+      setIsRandomAudio(false);
+      return;
     }
 
-    setActiveAudio(audios[current.index + 1 ])
-    setIsRandomAudio(false)
-  }
+    setActiveAudio(audios[current.index + 1]);
+    setIsRandomAudio(false);
+  };
 
   const onNextHandler = (current) => {
-    if (current.index === 4){
-      setActiveAudio(audios[0])
-      setIsRandomAudio(false)
-      return
+    if (current.index === 4) {
+      setActiveAudio(audios[0]);
+      setIsRandomAudio(false);
+      return;
     }
 
-    setActiveAudio(audios[current.index + 1 ])
-    setIsRandomAudio(false)
-  }
+    setActiveAudio(audios[current.index + 1]);
+    setIsRandomAudio(false);
+  };
 
   const onPreviousHandler = () => {
-    setIsRandomAudio(!isRandomAudio)
-  }
+    setIsRandomAudio(!isRandomAudio);
+  };
 
-  useEffect( () => { console.log({ isRandomAudio})}, [isRandomAudio])
+  useEffect(() => {
+    console.log({ isRandomAudio });
+  }, [isRandomAudio]);
 
-  
   return (
-    <main className='sm:flex sm:justify-center sm:items-center h-full w-full'>
-      <section className='grid w-full h-full grid-cols-1 grid-rows-2 xl:grid-cols-2 xl:grid-rows-1'>
-        {/* AUDIO PLAYER */}
-        <div className='relative w-full h-full flex justify-center items-center order-5'>
+    <main className='sm:flex sm:justify-center sm:items-center h-auto w-full'>
+      <section className='grid w-full h-auto grid-cols-1 grid-rows-2 xl:grid-cols-2 xl:grid-rows-1'>
+        <div className='relative w-full h-full flex justify-center items-center order-5 md:my-2 lg:m-0'>
           <div style={poadcastPlayerStyle} className='relative w-full h-full'>
-            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-8/12 h-auto flex flex-col justify-center items-center pl-4'>
+            <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-8/12 h-auto flex flex-col justify-center items-center pl-4 relative'>
               <img
-                className='w-10/12'
+                src={QueSoniamos}
+                className='-left-8 -top-20 absolute transform -rotate-12 z-20 w-36 sm:hidden'
+                alt='¿Que soñamos?'
+              />
+              <img
+                className='w-40 sm:w-60 lg:w-8/12 xl:w-10/12'
                 src={activeAudio.image}
                 alt={activeAudio.title}
               />
@@ -129,34 +134,32 @@ export default function Poadcast() {
                 {activeAudio.title}
               </p>
 
-              <div className='w-full'>
+              <div className='w-auto sm:w-3/4'>
                 <AudioPlayer
                   src={activeAudio.src}
                   onEnded={() => onEnded(activeAudio)}
-                  customVolumeControls={ [] }
+                  customVolumeControls={[]}
                   showSkipControls={true}
                   customAdditionalControls={[]}
                   onClickPrevious={() => onPreviousHandler(activeAudio)}
                   onClickNext={() => onNextHandler(activeAudio)}
                   customIcons={{
-                    pause: <Pause className="h-10 w-10" />,
-                    rewind: <Backward className="h-9 w-9" />,
-                    forward: <Forward className="h-9 w-9" />,
-                    next: <Loop className="h-7 w-7" />,
-                    previous: <Shuffle className="h-7 w-7" />
+                    pause: <Pause className='h-5 w-5 sm:h-10 sm:w-10' />,
+                    rewind: <Backward className='h-5 w-5 sm:h-9 sm:w-9' />,
+                    forward: <Forward className='h-5 w-5 sm:h-9 sm:w-9' />,
+                    next: <Loop className='h-5 w-5 sm:h-9 sm:w-9' />,
+                    previous: <Shuffle className='h-5 w-5 sm:h-9 sm:w-9' />,
                   }}
                 />
               </div>
-
             </div>
           </div>
         </div>
 
-        {/* INFORMATION COL */}
-        <div className='flex flex-col justify-center items-center order-5'>
+        <div className='flex flex-col justify-center items-center order-6 sm:order-1 xl:order-5'>
           <img
             src={QueSoniamos}
-            className='hidden md:block w-8/12'
+            className='hidden sm:block w-8/12'
             alt='¿Que soñamos?'
           />
 
@@ -166,19 +169,17 @@ export default function Poadcast() {
             víctimas de desplazamiento forzado. Escuchemos algunas de ellas:
           </p>
 
-          
-          <div className='flex flex-col justify-around items-center w-10/12'>
+          <div className='flex flex-col justify-around items-center w-10/12 m-4'>
             {audios.map((audio) => (
               <div
                 className='flex flex-row justify-start items-center cursor-pointer w-full text-left m-2'
                 onClick={() => setActiveAudio(audio)}
                 key={audio.title}>
-                <img src={PlayButton} className="w-10" alt='PlayButton' />
+                <img src={PlayButton} className='w-10' alt='PlayButton' />
                 <span className='font-sans px-4'>{audio.title}</span>
               </div>
             ))}
           </div>
-
         </div>
       </section>
     </main>
--- src/pages/Support/SupportMainSect.js
+++ src/pages/Support/SupportMainSect.js
@@ -28,44 +28,46 @@ function SupportMainSect() {
     }
 
     return (
-        <main className="h-auto w-full grid lg-2:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:grid-rows-min gap-4 p-0 sm:p-8 lg-2:p-4">
-            <div className="h-auto flex sm:w-full items-center lg-2:items-end justify-start sm:justify-center lg-2:justify-start p-4 sm:p-0">
-                <Hoverable src={ Concepts }
-                    HtmlTag="img" 
-                    alt="Concepto" 
-                    style={ style1 } 
-                    className="object-contain w-10/12 sm:w-3/4 sm:min-w-support-image cursor-pointer"
-                    onClick={()=> { push("/apoyo/conceptos") }}
-                    onHoverIn={() => setConceptsHover(true)} 
-                    onHoverOut={() => setConceptsHover(false)} 
-                    loading="lazy" />
-            </div>
-            <div className="flex h-auto w-full justify-end sm:items-center lg-2:items-start sm:justify-center lg-2:justify-start lg-2:col-span-2 sm:p-8 p-4">
-                <Hoverable src={ Statistics } 
-                    HtmlTag="img" 
-                    alt="Estadísticas" 
-                    style={ style2 } 
-                    className="object-contain w-10/12 lg-2:w-3/8 sm:min-w-support-image cursor-pointer" 
-                    onClick={()=> { push("/apoyo/estadisticas") }}
-                    onHoverIn={() => setStatisticsHover(true)} 
-                    onHoverOut={() => setStatisticsHover(false)} 
-                    loading="lazy" />
-            </div>
-            <div className="bg-blue-900 sm:bg-transparent h-auto w-full lg-2:pr-8 lg-2:col-span-2 p-3 sm:p-0 row-start-4 row-end-5 sm:row-start-auto sm:row-end-auto">
-                <p className="h-auto text-justify sm:mt-6 sm:pl-8 text-sm sm:text-base lg-2:pl-0 leading-6 text-white sm:text-black-700 font-acumin p-4 sm:p-0">Luego de la sentencia 411-2017 de la Corte Suprema de Justicia de El Salvador, el Estado se vio obligado a reconocer y legislar en favor de las víctimas de desplazamiento forzado por violencia. Tres años después, se aprobó una ley que carece de reglamento. Aunque existen acuerdos internacionales, como la Convención sobre Refugiados de 1951 y las Declaraciones de Cartagena y de Brasil, en El Salvador son documentos muertos. ¿A qué se enfrentan las familias cuando buscan apoyo de parte del Estado?</p>
-            </div>
-            <div className="flex sm:w-full items-center lg-2:items-start justify-start sm:justify-center lg-2:justify-start row-start-3 row-end-4 sm:row-start-auto sm:row-end-auto p-4 sm:p-0">
-                <Hoverable src={ Report } 
-                    HtmlTag="img" 
-                    alt="Reporte" 
-                    style={ style3 } 
-                    className="object-contain w-10/12 sm:w-3/4 sm:min-w-support-image cursor-pointer"
-                    onClick={()=> { push("/apoyo/reportajes") }}
-                    onHoverIn={() => setReportHover(true)} 
-                    onHoverOut={() => setReportHover(false)} 
-                    loading="lazy" />
-            </div>
-        </main>
+        <div className="h-full w-full flex items-center">
+            <main className="h-auto w-full grid lg-2:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:grid-rows-min gap-4 p-0 sm:p-8 lg-2:p-4">
+                <div className="h-auto flex sm:w-full items-center lg-2:items-end justify-start sm:justify-center lg-2:justify-start p-4 sm:p-0">
+                    <Hoverable src={ Concepts }
+                        HtmlTag="img" 
+                        alt="Concepto" 
+                        style={ style1 } 
+                        className="object-contain w-10/12 sm:w-3/4 sm:min-w-support-image cursor-pointer"
+                        onClick={()=> { push("/apoyo/conceptos") }}
+                        onHoverIn={() => setConceptsHover(true)} 
+                        onHoverOut={() => setConceptsHover(false)} 
+                        loading="lazy" />
+                </div>
+                <div className="flex h-auto w-full justify-end sm:items-center lg-2:items-start sm:justify-center lg-2:justify-start lg-2:col-span-2 sm:p-8 p-4">
+                    <Hoverable src={ Statistics } 
+                        HtmlTag="img" 
+                        alt="Estadísticas" 
+                        style={ style2 } 
+                        className="object-contain w-10/12 lg-2:w-3/8 sm:min-w-support-image cursor-pointer" 
+                        onClick={()=> { push("/apoyo/estadisticas") }}
+                        onHoverIn={() => setStatisticsHover(true)} 
+                        onHoverOut={() => setStatisticsHover(false)} 
+                        loading="lazy" />
+                </div>
+                <div className="bg-blue-900 sm:bg-transparent h-auto w-full lg-2:pr-8 lg-2:col-span-2 p-3 sm:p-0 row-start-4 row-end-5 sm:row-start-auto sm:row-end-auto">
+                    <p className="h-auto text-justify sm:mt-6 sm:pl-8 text-sm sm:text-base lg-2:pl-0 leading-6 text-white sm:text-black-700 font-acumin p-4 sm:p-0">Luego de la sentencia 411-2017 de la Corte Suprema de Justicia de El Salvador, el Estado se vio obligado a reconocer y legislar en favor de las víctimas de desplazamiento forzado por violencia. Tres años después, se aprobó una ley que carece de reglamento. Aunque existen acuerdos internacionales, como la Convención sobre Refugiados de 1951 y las Declaraciones de Cartagena y de Brasil, en El Salvador son documentos muertos. ¿A qué se enfrentan las familias cuando buscan apoyo de parte del Estado?</p>
+                </div>
+                <div className="flex sm:w-full items-center lg-2:items-start justify-start sm:justify-center lg-2:justify-start row-start-3 row-end-4 sm:row-start-auto sm:row-end-auto p-4 sm:p-0">
+                    <Hoverable src={ Report } 
+                        HtmlTag="img" 
+                        alt="Reporte" 
+                        style={ style3 } 
+                        className="object-contain w-10/12 sm:w-3/4 sm:min-w-support-image cursor-pointer"
+                        onClick={()=> { push("/apoyo/reportajes") }}
+                        onHoverIn={() => setReportHover(true)} 
+                        onHoverOut={() => setReportHover(false)} 
+                        loading="lazy" />
+                </div>
+            </main>
+        </div>
     )
 }
 
