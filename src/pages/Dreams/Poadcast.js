fe1ca220244fc458499ba8627dbf1a9604702a23
massana2110 <00072416@uca.edu.sv>
2021-09-16 23:48:18 -0600

    Apply react audio player component

--- src/pages/Dreams/Poadcast.js
+++ src/pages/Dreams/Poadcast.js
@@ -1,17 +1,29 @@
 import React, {useState} from 'react';
 
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
 //assets
 import Papel from '../../assets/images/Dreams/Poadcast/Papel.png';
 import QueSoniamos from '../../assets/images/Dreams/Poadcast/quesonamos.png';
 import PlayButton from '../../assets/images/Dreams/Poadcast/play1.png';
-import DefaultAudio from '../../assets/images/Dreams/Poadcast/default-audio.svg';
 
 import NuevaOportunidadImg from '../../assets/images/Dreams/Poadcast/una-nueva-oportunidad.png';
 import PermisoImg from '../../assets/images/Dreams/Poadcast/cuando-tenga-permiso.png';
 import VidaNormalImg from '../../assets/images/Dreams/Poadcast/deseo-vida-normal.png';
+import PazTranquilidadImg from '../../assets/images/Dreams/Poadcast/paz-tranqulidad.png';
+import SalirAdelanteImg from '../../assets/images/Dreams/Poadcast/tenemos-que-salir-adelante.png';
+
+//audio player icons
+import PlayIcon from '../../assets/images/Dreams/Poadcast/play1.png'
+import BackwardIcon from '../../assets/images/Dreams/Poadcast/Backward.png'
 
 // audios
 import NuevaOportunidadAudio from '../../assets/audios/Gladis_nueva_oportunidad_VM.mp3';
+import VidaNormalAudio from '../../assets/audios/Joaquin_vida_normal_como_antes_VM.mp3';
+import PazTranquilidadAudio from '../../assets/audios/Lucia_paz_tranquilidad_VM.mp3';
+import PermisoAudio from '../../assets/audios/Marcos_cuando_tenga_el_permiso_de_trabajar_VM.mp3';
+import SalirAdelanteAudio from '../../assets/audios/Maritza_tenemos_que_salir_adelante_VM.mp3';
 
 const poadcastPlayerStyle = {
   backgroundImage: `url(${Papel})`,
@@ -21,8 +33,7 @@ const poadcastPlayerStyle = {
 };
 
 export default function Poadcast() {
-  const [activeAudio, setActiveAudio] = useState({});
-
+  
   const audios = [
     {
       image: NuevaOportunidadImg,
@@ -31,25 +42,27 @@ export default function Poadcast() {
     },
     {
       image: VidaNormalImg,
-      src: '',
+      src: VidaNormalAudio,
       title: '"Deseo llevar una vida normal, como antes"',
     },
     {
-      image: '',
-      src: '',
+      image: PazTranquilidadImg,
+      src: PazTranquilidadAudio,
       title: '"Paz, tranquilidad y seguridad"',
     },
     {
-      image: '',
-      src: '',
+      image: PermisoImg,
+      src: PermisoAudio,
       title: '"Cuando tenga el permiso de trabajar, luchar por mi vida"',
     },
     {
-      image: '',
-      src: '',
+      image: SalirAdelanteImg,
+      src: SalirAdelanteAudio,
       title: '"Tenemos que salir adelante porque nos tenemos la una a la otra"',
     },
   ];
+
+  const [activeAudio, setActiveAudio] = useState(audios[0]);
 
   return (
     <main className="sm:flex sm:min-w-0 lg:min-w-min sm:justify-center sm:items-center h-full">
@@ -57,7 +70,7 @@ export default function Poadcast() {
         {/* AUDIO PLAYER */}
         <div
           style={poadcastPlayerStyle}
-          className="relative w-full flex flex-col items-center"
+          className="relative w-full h-full flex flex-col items-center"
         >
           <img
             src={QueSoniamos}
@@ -65,18 +78,34 @@ export default function Poadcast() {
             alt="¿Que soñamos?"
           />
 
-          {Object.keys(activeAudio) > 0 ? (
-            <img src={activeAudio.image} alt={activeAudio.title} />
-          ) : (
-            <div className="audio-player-image-default mt-20 ml-4 md:mt-40 md:ml-4"></div>
-          )}
+          <div className="flex flex-col justify-center items-center w-full h-auto max-h-40 mt-48 pl-10">
+            <img
+              className="w-1/3 h-full"
+              src={activeAudio.image}
+              alt={activeAudio.title}
+            />
 
-          <p className="ml-4 mt-2">{activeAudio.title}</p>
+            <p className="ml-4 mt-2">{activeAudio.title}</p>
+
+            <div className="mx-auto w-3/4">
+              <AudioPlayer
+                src={activeAudio.src}
+                customIcons={{
+                  play: PlayIcon,
+                  previous: BackwardIcon,
+                }}
+              />
+            </div>
+          </div>
         </div>
 
         {/* INFORMATION COL */}
         <div className="flex flex-col justify-center items-center md:ml-8 max-w-xl">
-          <img src={QueSoniamos} className="hidden md:block" alt="¿Que soñamos?" />
+          <img
+            src={QueSoniamos}
+            className="hidden md:block"
+            alt="¿Que soñamos?"
+          />
 
           <p className="text-justify mb-4">
             Mientras se encuentran en proceso de espera y reubicación las
@@ -88,6 +117,7 @@ export default function Poadcast() {
             <div
               onClick={() => setActiveAudio(audio)}
               className="flex mt-4 justify-start items-center cursor-pointer w-full"
+              key={audio.title}
             >
               <img src={PlayButton} width="50" alt="PlayButton" />
               <span className="ml-2">{audio.title}</span>
