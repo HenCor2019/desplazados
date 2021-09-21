c981e235fed6cbf58b26f35a0e78a1f3995b1621
Henry Cortez <Henry200amaya@gmail.com>
2021-09-21 14:32:17 -0600

    Fix comic

--- src/Components/Comic/Comic.js
+++ src/Components/Comic/Comic.js
@@ -1,34 +1,33 @@
 import FlipPage from 'react-flip-page'
 
-export default function ComicName(props) {
-  const { width, height, ref, images } = props
+export default function CompleteComic(props) {
+  const { width, height, refFlip, images } = props
 
   return (
-        <div className="w-auto mx-auto lg:mx-0 h-auto self-start mb-3 sm:mb-0 flex flex-col justify-center items-center content-center">
-          <FlipPage
-            orientation="horizontal"
-            animationDuration={1000}
-            width={width}
-            height={height}
-            ref={ref}
-            className="p-2"
+    <div className="w-auto mx-auto lg:mx-0 h-auto self-start mb-3 sm:mb-0 flex flex-col justify-center items-center content-center">
+      <FlipPage
+        orientation="horizontal"
+        animationDuration={1000}
+        width={width}
+        height={height}
+        ref={refFlip}
+        className="p-2"
+      >
+        {images.map((comic) => (
+          <div
+            key={comic.index}
+            className="w-full h-full bg-transparent bg-background "
           >
-            {images.map((comic) => (
-              <div
-                key={comic.index}
-                className="w-full h-full bg-transparent bg-background "
-              >
-                <img
-                  key={comic.index}
-                  src={comic.src}
-                  className="w-full h-full p-3 sm:py-1 sm:px-0"
-                  loading="lazy"
-                  alt={`imagen ${comic.index}`}
-                />
-              </div>
-            ))}
-          </FlipPage>
-        </div>
-
+            <img
+              key={comic.index}
+              src={comic.src}
+              className="w-full h-full p-3 sm:py-1 sm:px-0"
+              loading="lazy"
+              alt={`imagen ${comic.index}`}
+            />
+          </div>
+        ))}
+      </FlipPage>
+    </div>
   )
 }
--- src/pages/Comic/FlipPage.js
+++ src/pages/Comic/FlipPage.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
-import { comicImages } from "../../constants/Galleries";
-import { useConfigContext } from "../../contexts/ConfigContext";
-import { LazySection } from "../../HOC/LazySection/LazySection";
-import Dropdown from "../../Components/Dropdown/Dropdown";
-import Button from "../../Components/Comic/Button";
-import InnerButtons from "../../Components/Comic/InnerButtons";
+import { useEffect, useState } from 'react'
+import { comicImages } from '../../constants/Galleries'
+import { useConfigContext } from '../../contexts/ConfigContext'
+import { LazySection } from '../../HOC/LazySection/LazySection'
+import Dropdown from '../../Components/Dropdown/Dropdown'
+import Button from '../../Components/Comic/Button'
+import CompleteComic from '../../Components/Comic/Comic'
+import InnerButtons from '../../Components/Comic/InnerButtons'
 
 const Comic = () => {
   const {
@@ -13,87 +14,84 @@ const Comic = () => {
     isMonitor,
     isSmallMobile,
     isXlMonitor,
-    is2XlMonitor,
-  } = useConfigContext();
+    is2XlMonitor
+  } = useConfigContext()
 
-  const [flipPage, setFlipPage] = useState(null);
-  const [width, setWidth] = useState(650);
-  const [height, setHeight] = useState(510);
-  const [currentPage, setCurrentPage] = useState(null);
+  const [flipPage, setFlipPage] = useState(null)
+  const [width, setWidth] = useState(650)
+  const [height, setHeight] = useState(510)
+  const [currentPage, setCurrentPage] = useState(null)
 
   useEffect(
     () =>
       toggleSize({ isMobile, isMonitor, isTablet, isXlMonitor, is2XlMonitor }),
     [isMobile, isTablet, isMonitor, isSmallMobile, isXlMonitor, is2XlMonitor]
-  );
-
-
+  )
 
   const setNewSize = ({ newWidth, newHeight }) => {
-    setWidth(newWidth);
-    setHeight(newHeight);
-  };
+    setWidth(newWidth)
+    setHeight(newHeight)
+  }
 
   const toggleSize = ({ isMobile, isMonitor, isTablet }) => {
-    if (is2XlMonitor) setNewSize({ newWidth: 940, newHeight: 650 });
+    if (is2XlMonitor) setNewSize({ newWidth: 940, newHeight: 650 })
 
-    if (isXlMonitor) setNewSize({ newWidth: 850, newHeight: 555 });
+    if (isXlMonitor) setNewSize({ newWidth: 850, newHeight: 555 })
 
-    if (isMonitor) setNewSize({ newWidth: 730, newHeight: 580 });
+    if (isMonitor) setNewSize({ newWidth: 730, newHeight: 580 })
 
-    if (isTablet) setNewSize({ newWidth: 700, newHeight: 530 });
+    if (isTablet) setNewSize({ newWidth: 700, newHeight: 530 })
 
-    if (isMobile) setNewSize({ newWidth: 600, newHeight: 380 });
+    if (isMobile) setNewSize({ newWidth: 600, newHeight: 380 })
 
-    if (isSmallMobile) setNewSize({ newWidth: 330, newHeight: 310 });
-  };
+    if (isSmallMobile) setNewSize({ newWidth: 330, newHeight: 310 })
+  }
 
   const refFlipPage = (component) => {
-    setFlipPage(component);
-  };
+    setFlipPage(component)
+  }
 
-  const onPreviousPage = () => flipPage.gotoPreviousPage();
-  const onNextPage = () => flipPage.gotoNextPage();
+  const onPreviousPage = () => flipPage.gotoPreviousPage()
+  const onNextPage = () => flipPage.gotoNextPage()
 
   useEffect(() => {
-    if (!currentPage) return;
-    const [page] = currentPage;
-    flipPage.gotoPage(page.index);
-
-  }, [currentPage]);
+    if (!currentPage) return
+    const [page] = currentPage
+    flipPage.gotoPage(page.index)
+  }, [currentPage])
 
   return (
     <>
       <Dropdown
-        placeholder='Ir a página'
+        placeholder="Ir a página"
         options={comicImages.filter((image) => image.name !== undefined)}
         onChange={(values) => setCurrentPage(values)}
-        label='name'
-        value='index'
-        secondPlaceholder='Seleccionar'
+        label="name"
+        value="index"
+        secondPlaceholder="Seleccionar"
       />
 
-      <section className='comic-container w-full sm:h-full md:h-5/6 2xl:h-full flex flex-col lg:flex-row justify-center items-center'>
+      <section className="comic-container w-full sm:h-full md:h-5/6 2xl:h-full flex flex-col lg:flex-row justify-center items-center">
         <Button
           height={height}
-          margin='mr-5'
+          margin="mr-5"
           onClick={onPreviousPage}
-          rotate='-rotate-180'
+          rotate="-rotate-180"
         />
 
-        <Comic
+        <CompleteComic
           width={width}
           height={height}
-          ref={refFlipPage}
+          refFlip={refFlipPage}
           images={comicImages}
         />
 
         <InnerButtons onNext={onNextPage} onPrev={onPreviousPage} />
 
-        <Button height={height} margin='ml-5' onClick={onNextPage} />
+        <Button height={height} margin="ml-5" onClick={onNextPage} />
       </section>
     </>
-  );
-};
+  )
+}
 
-export default LazySection(Comic);
+export default LazySection(Comic)
