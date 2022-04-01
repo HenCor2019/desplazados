5c8e1fedd4d8992686aae0a8e06279e56318708a
HenCor2019 <henry200amaya@gmail.com>
2022-04-01 13:22:21 -0600

    :sparkles: Add video instead podcast audio player

--- src/dreams/components/poadcast/Poadcast.js
+++ src/dreams/components/poadcast/Poadcast.js
@@ -16,11 +16,12 @@ import { ReactComponent as Pause } from '../../../assets/svg/pause.svg';
 import { setAudios } from '../../constants/podcast';
 
 import '../../assets/css/padcast.css';
+import SimpleVideoPlayer from 'introduction/components/SimpleVideoPlayer';
 
 const poadcastPlayerStyle = {
   backgroundImage: `url(${Paper})`,
   backgroundPosition: 'center',
-  backgroundSize: 'auto',
+  backgroundSize: 'contain',
   backgroundRepeat: 'no-repeat',
   };
 
@@ -79,33 +80,12 @@ export default (function Poadcast() {
       <section className="grid grid-flow-row-dense w-full h-auto grid-cols-1 lg:grid-cols-3">
         <div className="relative w-full h-full grid grid-cols-1 grid-flow-row order-5 md:my-2 lg:m-0 lg:col-span-2">
           <div className="flex-col items-center w-fit p-2/3">
-            <div style={poadcastPlayerStyle} className="w-full h-2/3">
-              <div className="audio-player flex justify-center items-center h-full w-full flex-col lg:flex-row">
-                <div className='h-1/2 w-1/2 mb-4 lg:mb-0'>
-                <img
-                  className="object-contain w-full h-full"
-                  src={audios[index].image}
-                  alt={audios[index].title}
-                />
-                </div>
-                <div className="audio-player-container w-2/3 sm:w-1/3 lg:w-25 lg:pr-12">
-                <p className="font-sans text-sm md:text-sm lg:text-base text-center pb-5 md:pb-0">
-                  {audios[index].title}
-                </p>
-                  <AudioPlayer
-                    autoPlay
-                    autoPlayAfterSrcChange
-                    src={activeAudio.src}
-                    onEnded={onEnded}
-                    showJumpControls={false}
-                    showSkipControls
-                    customAdditionalControls={[RHAP_UI.LOOP]}
-                    customVolumeControls={[RHAP_UI.VOLUME]}
-                    onClickPrevious={onPrevious}
-                    onClickNext={onNext}
-                    customIcons={customIcons}
+            <div style={poadcastPlayerStyle} className="flex justify-center items-center w-full h-2/3 bg-contain">
+              <div className='flex justify-center items-center w-11/12 py-4 px-3 sm:h-90/100 sm:px-14 sm:py-4 md:w-4/6 md:h-4/5 md:py-0 lg:w-full lg:px-6 xl:w-full xl:ml-24 xl:mr-16'>
+                <SimpleVideoPlayer
+                  title='titulo'
+                  embedId='YRz3OONf_j8'
                   />
-                </div>
               </div>
             </div>
               <div className="flex flex-col justify-center items-center content-center w-10/12 ml-6 lg:ml-20">
