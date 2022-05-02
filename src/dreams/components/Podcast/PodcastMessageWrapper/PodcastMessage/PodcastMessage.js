79df2509e6f426e43ecba7c08c774f5d88c8ec74
HenCor2019 <henry200amaya@gmail.com>
2022-05-02 10:04:39 -0600

    :ambulance: Hotfix message and principal image
    Message and image not working with a diferente language

--- src/dreams/components/Podcast/PodcastMessageWrapper/PodcastMessage/PodcastMessage.js
+++ src/dreams/components/Podcast/PodcastMessageWrapper/PodcastMessage/PodcastMessage.js
@@ -1,9 +1,9 @@
 import styles from './PodcastMessage.module.css'
-export default function PodcastMessage() {
+export default function PodcastMessage({ message }) {
   return (
     <section className={styles.wrapper}>
       <p className={styles.message}>
-        Mientras se encuentran en proceso de espera y reubicación las perspectivas de esperanza y futuro siempre están latentes entre las víctimas de desplazamiento forzado. Escuchemos algunas de ellas:
+        { message }
       </p>
     </section>
   )
