95b3cfd43bb461d4b063fdbe26ad0ccb93f1f93e
HenCor2019 <henry200amaya@gmail.com>
2022-05-03 20:09:01 -0600

    :ambulance: Fix bad reponsive for flags

--- src/shared/components/Section/Landscape/Landscape.js
+++ src/shared/components/Section/Landscape/Landscape.js
@@ -8,7 +8,7 @@ import classes from './Landscape.module.scss';
 const aClasses = 'px-4 font-sans text-gray-500 hover:text-gray-200 text-base';
 
 function Landscape(props) {
-  const paddindWrapper = i18next.language === 'en' ? 'pt-3' : 'pt-3'
+  //const paddindWrapper = i18next.language === 'en' ? 'pt-3' : 'pt-3'
 
   const linksComponents = props.links.map((link) => (
     <NavLink
@@ -32,13 +32,13 @@ function Landscape(props) {
           src={props.banner}
         />
       </div>
-      <div className="flex flex-col gap-2 flex-rigid max-h-section h-full overflow-y-scroll">
+      <div className="flex flex-col gap-2 flex-rigid max-h-section h-full overflow-y-scroll ">
         <nav
           style={{ backgroundColor: '#e8e8e7e6' }}
-          className="main-nav max-w-nav overflow-hidden flex justify-end p-2 pb-7 sticky top-0 z-20"
+          className="main-nav max-w-nav overflow-visible flex justify-end p-2 pb-7 sticky top-0 z-20"
         >
           {linksComponents}
-          <div className={`flex justify-center items-center ${paddindWrapper}`}>
+          <div className={`flex justify-center items-center self-start`}>
             <TranslateButtons type="navbar-landscape" />
           </div>
         </nav>
